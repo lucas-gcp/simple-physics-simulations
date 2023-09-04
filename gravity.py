a01104fd1ed0bd41728b6e7571111aaf1f751a86
Lucas Guido <lucasguidocp@gmail.com>
2023-09-04 20:49:08 -0300

    Added Body class 	Added draw_body function

--- gravity.py
+++ gravity.py
@@ -1,1 +1,7 @@
-# implementation for 2 bodies
+# Implementation of the Body class
+
+class Body:
+    def __init__(self, xpos,ypos, radius):
+        self.xpos = xpos
+        self.ypos = ypos
+        self.radius = radius
--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import pygame
+from gravity import Body
 
 pygame.init()
+
+# Function to draw object from Body class
+def draw_body(surface, color, body):
+    pygame.draw.circle(screen, 'white', (body.xpos, body.ypos), body.radius)
 
 screenWidth = 400
 screenHeight = 400
 screen = pygame.display.set_mode([screenWidth, screenHeight])
+
+# Test bodies
+body_1 = Body(100,100, 5)
+body_2 = Body(200, 200, 3)
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
-    screen.fill ((255,255,255))
+
+    screen.fill ('black')
+    # Test the draw_body function
+    draw_body(screen, 'white', body_1)
+    draw_body(screen, 'red', body_2)
     pygame.display.update()
 
 pygame.quit()
