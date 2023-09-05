c7343c32d54b4c3410a4752a5242a62ea5206cf0
Lucas Guido <lucasguidocp@gmail.com>
2023-09-04 21:46:52 -0300

    Added speed and acceleration 	Tests on main

--- gravity.py
+++ gravity.py
@@ -1,7 +1,19 @@
 # Implementation of the Body class
 
 class Body:
-    def __init__(self, xpos,ypos, radius):
+    def __init__(self, xpos, ypos, radius, xspeed, yspeed, xaccel, yaccel):
         self.xpos = xpos
         self.ypos = ypos
         self.radius = radius
+        self.xspeed = xspeed
+        self.yspeed = yspeed
+        self.xaccel = xaccel
+        self.yaccel = yaccel
+    
+    def calc_speed(self):
+        self.xspeed += self.xaccel
+        self.yspeed += self.yaccel
+
+    def move(self):
+        self.xpos += self.xspeed
+        self.ypos += self.yspeed
--- main.py
+++ main.py
@@ -10,21 +10,26 @@ def draw_body(surface, color, body):
 screenWidth = 400
 screenHeight = 400
 screen = pygame.display.set_mode([screenWidth, screenHeight])
+clock = pygame.time.Clock()
 
 # Test bodies
-body_1 = Body(100,100, 5)
-body_2 = Body(200, 200, 3)
+body_1 = Body(100,100, 5, 0, 0, 0.1, 0.1)
+body_2 = Body(200, 200, 3, 1, 1, 0, 0)
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            
+    body_1.calc_speed()
+    body_1.move()
 
     screen.fill ('black')
     # Test the draw_body function
     draw_body(screen, 'white', body_1)
     draw_body(screen, 'red', body_2)
     pygame.display.update()
+    clock.tick(60)
 
 pygame.quit()
