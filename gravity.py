793bdcbd24a0f5f850f0100bfca99bff0f582f8a
Lucas Guido <lucasguidocp@gmail.com>
2023-09-04 23:51:47 -0300

    Added -wrong-gravitation calculations

--- gravity.py
+++ gravity.py
@@ -1,15 +1,28 @@
+import math
 # Implementation of the Body class
 
 class Body:
-    def __init__(self, xpos, ypos, radius, xspeed, yspeed, xaccel, yaccel):
+    def __init__(self, xpos, ypos, mass, radius, xspeed, yspeed, xaccel, yaccel):
         self.xpos = xpos
         self.ypos = ypos
+        self.mass = mass
         self.radius = radius
         self.xspeed = xspeed
         self.yspeed = yspeed
         self.xaccel = xaccel
         self.yaccel = yaccel
-    
+
+    # TODO fix gravitation calculation
+    def calc_accel(self, body):
+        xdistance = self.xpos - body.xpos
+        ydistance = self.ypos - body.ypos
+        distance = (xdistance**2 + ydistance**2)**0.5
+        accel = 6.67*10**(-11) * (body.mass/distance**2)
+        xangle = math.atan(ydistance/xdistance)
+        yangle = math.atan(xdistance/ydistance)
+        self.xaccel = math.cos(xangle)*accel
+        self.yaccel = math.cos(yangle)*accel
+
     def calc_speed(self):
         self.xspeed += self.xaccel
         self.yspeed += self.yaccel
--- main.py
+++ main.py
@@ -7,14 +7,14 @@ pygame.init()
 def draw_body(surface, color, body):
     pygame.draw.circle(screen, 'white', (body.xpos, body.ypos), body.radius)
 
-screenWidth = 400
-screenHeight = 400
+screenWidth = 1000
+screenHeight = 1000
 screen = pygame.display.set_mode([screenWidth, screenHeight])
 clock = pygame.time.Clock()
 
 # Test bodies
-body_1 = Body(100,100, 5, 0, 0, 0.1, 0.1)
-body_2 = Body(200, 200, 3, 1, 1, 0, 0)
+body_1 = Body(100, 100, 10**12, 3, 0.2, 0, 0, 0)
+body_2 = Body(200, 200, 10**12, 3, 0, 0, 0, 0)
 
 running = True
 while running:
@@ -22,14 +22,18 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             
+    body_1.calc_accel(body_2)
     body_1.calc_speed()
     body_1.move()
+    body_2.calc_accel(body_1)
+    body_2.calc_speed()
+    body_2.move()
 
     screen.fill ('black')
     # Test the draw_body function
     draw_body(screen, 'white', body_1)
     draw_body(screen, 'red', body_2)
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(20)
 
 pygame.quit()
