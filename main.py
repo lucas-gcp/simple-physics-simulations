fe8fc6057a2bd1b099fbde35c14c8c3df4243cca
grecoww <132113259+grecoww@users.noreply.github.com>
2023-09-06 17:25:56 -0300

    Cleaning the code
    added tickrate variable so we can make the velocity change to be constant indepently to the tickrate value later
    
    on xdir and ydir we can delete de (-1) if we invert the terms

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ def draw_body(surface, color, body):
     pygame.draw.circle(surface, color, (body.xpos, body.ypos), body.radius)
 
 
-screenWidth = 1000
-screenHeight = 1000
+screenWidth, screenHeight = 1000, 1000
 screen = pygame.display.set_mode([screenWidth, screenHeight])
 clock = pygame.time.Clock()
 
@@ -19,6 +18,7 @@ body_1 = Body(500, 649.6, 5.9722*10**24, 3, 2.978, 0, 0, 0)
 body_2 = Body(500, 500, 1.98*10**31, 5, 0, 0, 0, 0)
 
 running = True
+tickrate = 60
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -37,6 +37,6 @@ while running:
     draw_body(screen, 'blue', body_1)
     draw_body(screen, 'yellow', body_2)
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(tickrate)
 
 pygame.quit()
