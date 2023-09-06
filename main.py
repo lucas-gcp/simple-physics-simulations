8d4ae9f777056f80af775bf376da0cefc8e88a69
Lucas Guido <lucasguidocp@gmail.com>
2023-09-06 18:54:56 -0300

    Partially finished n-body model

--- main.py
+++ main.py
@@ -18,12 +18,12 @@ def update_body(bodies_list):
     for b in bodies_list:
         calculate_acceleration(b, filter(lambda x: x != b, bodies_list))
         b.calc_speed()
+    for b in bodies_list:
         b.move()
         b.clear_accel()
 
 
-screenWidth = 1000
-screenHeight = 1000
+screenWidth, screenHeight = 1000, 1000
 screen = pygame.display.set_mode([screenWidth, screenHeight])
 clock = pygame.time.Clock()
 
@@ -36,10 +36,11 @@ body_1 = Body(500, 649.6, 5.9722*10**24, 3, 2.978, 0, 0, 0)
 bodies.append(body_1)
 body_2 = Body(500, 500, 1.98*10**31, 5, 0, 0, 0, 0)
 bodies.append(body_2)
-body_3 = Body(500, 650, 7.34767309*10**22, 2, 2.97, 0, 0, 0)
+body_3 = Body(500, 650, 7.34767309*10**22, 2, 2.95, 0, 0, 0)
 bodies.append(body_3)
 
 running = True
+tickrate = 60
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -55,6 +56,6 @@ while running:
     draw_body(screen, 'yellow', body_2)
     draw_body(screen, 'white', body_3)
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(tickrate)
 
 pygame.quit()
