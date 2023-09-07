cf7b27b4513c87543ced3ecae49063adc84c773b
Lucas Guido <lucasguidocp@gmail.com>
2023-09-06 21:06:51 -0300

    Finished

--- main.py
+++ main.py
@@ -4,11 +4,6 @@ from gravity import Body
 pygame.init()
 
 
-# Function to draw object from Body class
-def draw_body(surface, color, body):
-    pygame.draw.circle(surface, color, (body.xpos, body.ypos), body.radius)
-
-
 def calculate_acceleration(body, bodies_list):
     for b in bodies_list:
         body.calc_accel(b)
@@ -23,21 +18,28 @@ def update_body(bodies_list):
         b.clear_accel()
 
 
+# Function to draw object from Body class
+def draw_body(surface, color, body):
+    pygame.draw.circle(surface, color, (body.xpos, body.ypos), body.radius)
+
+
 screenWidth, screenHeight = 1000, 1000
 screen = pygame.display.set_mode([screenWidth, screenHeight])
 clock = pygame.time.Clock()
 
-earth = pygame.image.load(R"D:\DOWNLOADS\terra.png")
+earth = pygame.image.load(R".\resources\earth.png")
 earth = pygame.transform.scale(earth, (15, 15))
 
 bodies = []
 # Test bodies
-body_1 = Body(500, 649.6, 5.9722*10**24, 3, 2.978, 0, 0, 0)
+body_1 = Body(300, 200, 5.9722*10**12, 3, 1.4, 0.15, 0, 0)
 bodies.append(body_1)
-body_2 = Body(500, 500, 1.98*10**31, 5, 0, 0, 0, 0)
+body_2 = Body(280, 300, 1.98*10**12, 5, 0, 0, 0, 0)
 bodies.append(body_2)
-body_3 = Body(500, 650, 7.34767309*10**22, 2, 2.95, 0, 0, 0)
+body_3 = Body(450, 340, 7.34767309*10**12, 2, 0.9, -1, 0, 0)
 bodies.append(body_3)
+body_4 = Body(600, 270, 7.34767309*10**12, 4, -0.9, 1.9, 0, 0)
+bodies.append(body_4)
 
 running = True
 tickrate = 60
@@ -55,6 +57,7 @@ while running:
     draw_body(screen, 'blue', body_1)
     draw_body(screen, 'yellow', body_2)
     draw_body(screen, 'white', body_3)
+    draw_body(screen, 'green', body_4)
     pygame.display.update()
     clock.tick(tickrate)
 
