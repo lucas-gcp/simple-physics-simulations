0ecc4f875d8e850cc3b2cc7949218c5f366f6402
Lucas Guido <lucasguidocp@gmail.com>
2023-10-15 22:50:49 -0300

    Error handling

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import gravityevent
 simulationlist = ["gravity", "collisions"]
 print(simulationlist)
 
-indexSimulation = input("Wich simulation to start? ")
+indexSimulation = input("Which simulation to start? ").lower()
 
 #if user wants to evoke gravity simulation:
 if indexSimulation == "gravity":
     gravityevent.gravity_run()
+else:
+    print('No simulation with this name')
