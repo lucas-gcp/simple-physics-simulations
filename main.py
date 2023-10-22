a636e949dd9d90f3d2ef9de1a24ad5ffea902865
Lucas735 <61996520+Lucas735@users.noreply.github.com>
2023-10-22 15:15:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #main.py will be the pygame menu to select (through GUI) the requested simulation
 import gravityevent
+import collision_event
 
 simulationlist = ["gravity", "collisions"]
 print(simulationlist)
@@ -9,5 +10,7 @@ indexSimulation = input("Which simulation to start? ").lower()
 #if user wants to evoke gravity simulation:
 if indexSimulation == "gravity":
     gravityevent.gravity_run()
+elif indexSimulation == "collisions":
+    collision_event.collision_run()
 else:
     print('No simulation with this name')
