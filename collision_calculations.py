10127aba810c0bc7585fe00c0fdbf2f409ccfa7b
Lucas Guido <lucasguidocp@gmail.com>
2023-11-02 00:08:13 -0300

    Error handling

---
+++ collision_calculations.py
@@ -0,0 +1,8 @@
+class Block:
+    def __init__(self, xpos, ypos, xsize, ysize, speed, mass):
+        self.xpos = xpos
+        self.ypos = ypos
+        self.xsize = xsize
+        self.ysize = ysize
+        self.speed = speed
+        self.mass = mass
--- collision_event.py
+++ collision_event.py
@@ -1,35 +1,41 @@
-
 import pygame
 import os
+from collision_calculations import Block
+
+def calculate_velocity(block1, block2):
+    block1_speed_after = abs((((block1.mass - block2.mass) / (block1.mass + block2.mass)) * block1.speed) + (
+        ((2 * block2.mass) / (block1.mass + block2.mass)) * block2.speed))
+    block2_speed_after = abs(
+        ((block1.mass * block1.speed + block2.mass * block2.speed) - block1.mass * block1_speed_after) / block2.mass)
+    block1.speed = -(block1_speed_after)
+    block2.speed = -(block2_speed_after)
 
 def collision_run():
-    #Screen settings
+    # Screen settings
     pygame.init()
     WIDTH, HEIGHT = 900, 500
     WIN = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption('Collisions simulator')
     FPS = 60
 
-    #Images settings
+    # Images settings
     background = pygame.transform.scale(pygame.image.load(os.path.join('resources', 'collision_background.jpg')), (WIDTH, HEIGHT))
 
-    #Block1 configs
-    block1 = pygame.Surface((50, 50))
-    pygame.Surface.fill(block1, 'red')
-    block1_rect = block1.get_rect(topright = ((WIDTH/4), (HEIGHT / 2 - 55)))
+    # Block1 configs
+    block1 = Block(WIDTH/4, HEIGHT / 2 - 55, 50, 50, 2, 2)
+    block1_surface = pygame.Surface((block1.xsize, block1.ysize))
+    pygame.Surface.fill(block1_surface, 'red')
+    block1_rect = block1_surface.get_rect(topright = (block1.xpos, block1.ypos))
 
-    block1_speed = 2
-    block1_mass = 2
-    momentum_block1 = block1_mass * block1_speed
+    momentum_block1 = block1.mass * block1.speed
 
-    #Block2 configs
-    block2 = pygame.Surface((50, 50))
-    pygame.Surface.fill(block2, 'black')
-    block2_rect = block2.get_rect(topright = ((WIDTH - WIDTH/4), (HEIGHT / 2 - 55)))
+    # Block2 configs
+    block2 = Block(WIDTH - WIDTH/4, HEIGHT/2 - 55, 50, 50, 4, 1)
+    block2_surface = pygame.Surface((block2.xsize, block2.ysize))
+    pygame.Surface.fill(block2_surface, 'black')
+    block2_rect = block2_surface.get_rect(topright = (block2.xpos, block2.ypos))
 
-    block2_speed = 4
-    block2_mass = 1
-    momentum_block2 = block2_mass * block2_speed
+    momentum_block2 = block2.mass * block2.speed
 
 
     clock = pygame.time.Clock()
@@ -46,24 +52,21 @@ def collision_run():
 
 
         #Blit block 1
-        block1_rect.x += block1_speed
-        WIN.blit(block1, block1_rect)
+        block1_rect.x += block1.speed
+        WIN.blit(block1_surface, block1_rect)
 
         #Blit block 2
-        block2_rect.x += -(block2_speed)
-        WIN.blit(block2, block2_rect)
+        block2_rect.x += -(block2.speed)
+        WIN.blit(block2_surface, block2_rect)
 
         trial = 0
         if block1_rect.colliderect(block2_rect) and trial == 0:
             trial = 1
-            print('Block 1 Speed: ', block1_speed)
-            print('Block 2 Speed: ', block2_speed)
-            block1_speed_after = abs((((block1_mass - block2_mass) / (block1_mass + block2_mass)) * block1_speed) + (((2*block2_mass) / (block1_mass + block2_mass)) * block2_speed))
-            print('Block 1 Speed after: ', block1_speed_after)
-            block2_speed_after = abs(((block1_mass * block1_speed + block2_mass * block2_speed) - block1_mass * block1_speed_after) / block2_mass)
-            print('Block 2 Speed after: ', block2_speed_after)
-            block1_speed = -(block1_speed_after)
-            block2_speed = -(block2_speed_after)
+            print('Block 1 Speed: ', block1.speed)
+            print('Block 2 Speed: ', block2.speed)
+            calculate_velocity(block1, block2)
+            print('Block 1 Speed after: ', block1.speed)
+            print('Block 2 Speed after: ', block2.speed)
 
 
 
